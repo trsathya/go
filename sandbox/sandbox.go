566c87fec3b36eeb362c798b96f5d9bc6f327896
Sathya <trsathya@gmail.com>
2015-07-04 14:18:59 -0400

    Flow control statements

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"math/cmplx"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -49,6 +50,24 @@ func needInt(x int) int { return x*10 + 1 }
 func needFloat(x float64) float64 {
 	return x * 0.1
 }
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
 func main() {
 	fmt.Println("Welcome to the playground!")
 	fmt.Println("The time is", time.Now())
@@ -122,5 +141,80 @@ func main() {
 	fmt.Println(needInt(Small))
 	fmt.Println(needFloat(Small))
 	fmt.Println(needFloat(Big))
+
+	sum1 := 0
+	for i := 0; i < 10; i++ {
+		sum1 += i
+	}
+	fmt.Println(sum1)
+
+	sum2 := 1
+	for sum2 < 1000 {
+		sum2 += sum2
+	}
+	fmt.Println(sum2)
+
+	// For is Go's "while"
+	sum3 := 1
+	for sum3 < 1000 {
+		sum3 += sum3
+	}
+	fmt.Println(sum3)
+
+	fmt.Println(sqrt(2), sqrt(-4))
+
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
+	//switch
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
+
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	fmt.Println(today, time.Saturday)
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	// defer
+	defer fmt.Println("world")
+	fmt.Println("hello")
+
+	// stacking defer
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 
 }
